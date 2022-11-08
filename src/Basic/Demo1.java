b09dfafa099cc506be53871cdc7e1bcf0c0b6dbf
Khanorkar <Khanorkar@DESKTOP-JM76D50>
2022-11-08 13:41:22 +0530

    First commit1

---
+++ src/Basic/Demo1.java
@@ -0,0 +1,19 @@
+package Basic;
+
+public class Demo1
+{
+public static void main(String[] args) 
+{
+	System.out.println("good moring");
+	
+	M1();
+}
+
+public static void M1()
+{
+	System.out.println("Hello");
+}
+
+
+
+}
